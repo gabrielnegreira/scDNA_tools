361ae04abd9b9ca6aa2e6d46bae5671b2178d4d8
gabrielnegreira <negreiragabriel@gmail.com>
2026-07-23 15:39:33 +0200

    makes an workflow example

--- workflow_example.r
+++ workflow_example.r
@@ -1,3 +1,34 @@
+#install packages####
+install_packages <- FALSE #set to TRUE to install required packages
+if(install_packages){
+  # Install CRAN packages
+  cran_packages <- c(
+    "matrixStats",
+    "dplyr",
+    "tidyr",
+    "tibble",
+    "forcats",
+    "ggplot2",
+    "plotly",
+    "mgcv",
+    "ggrepel",
+    "patchwork",
+    "hues",
+    "mixtools"
+  )
+  
+  install.packages(cran_packages)
+  
+  # Install Bioconductor package manager if needed
+  if (!requireNamespace("BiocManager", quietly = TRUE)) {
+    install.packages("BiocManager")
+  }
+  
+  # Install Bioconductor packages
+  bioc_packages <- c("rhdf5")
+  BiocManager::install(bioc_packages)
+}
+
 #load libraries and functions####
 #get the functions
 source("scDNA_functions.R")
@@ -5,56 +36,13 @@ source("scDNA_functions.R")
 #get the data####
 ##get the count matrix####
 count_matrix <- as.matrix(read.csv("count_matrix_example.csv", row.names = 1))
-#remove kDNA from count matrix 
-
-##make a dummy cells_metadata####
-cells_meta <- data.frame(barcode = colnames(count_matrix))
-
-#for now cells_meta is required to have a `cell_id` column with a numeric identifier of that cells. 
-# This was based on the cellranger-dna metadata.
-#I think this is actually not necessary so I might remove this requirement in the future. 
-#But for now, we create one:
-cells_meta$cell_id <- 1:nrow(cells_meta)
-
-#the cells metadata must have rownames matching the colnames of the count matrix!
-#this is important because plotting functions use the row names of the cells meta to match metadata with count values in the count matrix!
-rownames(cells_meta) <- cells_meta$barcode
-
-#confirm they match
-all(rownames(cells_meta) == colnames(count_matrix))
-
-#let's inspect cells metadata:
-head(cells_meta)
-
-##nmake a dummy bins metadata####
-##If we don't have information about the bins gc content, mappability, etc, we can create dummy values
-bins_meta <- data.frame(
-  start = NA,
-  end = NA,
-  bin = rownames(count_matrix),
-  chromosome = gsub("_.*", "", rownames(count_matrix)), #this gets the chromosome from the bin name
-  gc_content = rnorm(n = nrow(count_matrix), mean = 0.5, sd = 0.2),
-  mappability = 1,
-  is_mappable = TRUE
-)
-
-#we also need to set bins_meta rownames to the bins names in the count matrix
-##OBS: currently there will be no warning if this is not met. So I should fix it later.
-rownames(bins_meta) <- bins_meta$bin
-#confirm they match
-all(rownames(cells_meta) == colnames(count_matrix))
-
-#let's inspect bins_meta
-head(bins_meta)
+##get the bins metadata####
+bins_meta <- read.csv("bins_meta_example.csv")
 
 #build the scDNA obj####
 obj <- build_scDNAobj(
   count_matrix = count_matrix, 
-  cells_meta = cells_meta,
-  bins_meta = bins_meta,
-  mean_read_length = 400, #the average read length of the library. We can get that from the TapeStation profile. Only used to calculate the effective depth of coverage.
-  genome_size = 33000000 #also only used for calculating effective depth of coverage. Might remove these two in the future.
-)
+  bins_meta = bins_meta)
 
 #perform the analysis####
 ##we start by distinguishing background droplets from true cells
@@ -69,12 +57,13 @@ obj <- correct_counts(obj, vars_to_correct = "gc_content") #will correct counts 
 obj <- normalize_counts(obj)
 obj <- calc_cells_ICF(obj)
 obj <- tag_outlier_cells(obj, vars_to_check = c(ICCV = "upper", ICF_score = "upper"))
-obj <- calc_somy(obj, int_method = "round") #for gaussian mixuture models set it to "GMM" (but it might get stuck in a loop sometimes. Need to fix it)
+obj <- calc_somy(obj, int_method = "GMM") #for gaussian mixuture models set it to "GMM" (but it might get stuck in a loop sometimes, otherwise set it to "round")
 obj <- summarise_karyotypes(obj)
+
 #Visualize results####
 ##visualize somies####
 ## to visualize somies we can use the `plot_somies` function
-plot_somies(obj, matrix_to_plot = "int_somy_matrix")
+plot_somies(obj)
 
 ##visualize individual cells####
 ## we can use the `plot_cell` function to visualize a specific cell in an object
